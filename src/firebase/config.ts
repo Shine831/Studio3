43868220be046b142ff6f1768d1a0c2c3530ffce
Claude Baha <jcbaha58@gmail.com>
2026-01-20 03:14:49 +0000

    1. Même erreur quand je veux sign up avec Google.(Pareil pour le login)

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 import { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBz7pfxISNGT_6dxLDQpn0TI7-5nTBNSEg",
+  authDomain: "studio-8216247435-c5b55.firebaseapp.com",
+  projectId: "studio-8216247435-c5b55",
+  storageBucket: "studio-8216247435-c5b55.appspot.com",
+  messagingSenderId: "973507961662",
+  appId: "1:973507961662:web:94e215836b61182702026f",
 };
