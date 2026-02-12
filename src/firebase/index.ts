f6169111dc08f9750dfad774743c73c9ec2ca0d5
Claude Baha <jcbaha58@gmail.com>
2026-02-12 00:06:31 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/arg

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,26 +1,21 @@
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { initializeAuth, browserLocalPersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    const firebaseApp = initializeApp(firebaseConfig);
-    return getSdks(firebaseApp);
-  }
-
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+  return getSdks(app);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
   return {
     firebaseApp,
-    auth: initializeAuth(firebaseApp, { persistence: browserLocalPersistence }),
-    firestore: getFirestore(firebaseApp)
+    auth: getAuth(firebaseApp),
+    firestore: getFirestore(firebaseApp),
   };
 }
 
