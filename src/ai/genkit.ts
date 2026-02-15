c83efb11601b29de6a65aec8a7e8c6f711067a46
Claude Baha <jcbaha58@gmail.com>
2026-02-15 10:58:13 +0000

    Ça ne fait que persister et ça me dérange 11:55:04.207 Running build in

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,8 @@
 'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import nextPlugin from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI(), nextPlugin({})],
+  plugins: [googleAI()],
   model: 'googleai/gemini-pro',
 });
